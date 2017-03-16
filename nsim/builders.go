bce29964bd3093acc3f346a61f48607b82994be0
Andrew Romanenco <andrew@romanenco.com>
2017-03-16 14:04:22 -0400

    Add builder for ip packet.

--- nsim/builders.go
+++ nsim/builders.go
@@ -10,3 +10,45 @@ func ParseNetworkInterface(cidr string) *NetworkInterface {
 	}
 	return &NetworkInterface{ip, *net}
 }
+
+// IPPacketBuilder is a builder of a IP packet.
+type IPPacketBuilder struct {
+	destination net.IP
+	source      net.IP
+	ttl         int
+	protocol    string
+}
+
+// NewIPPacketBuilder returns a new ip packet builder.
+func NewIPPacketBuilder() *IPPacketBuilder {
+	return &IPPacketBuilder{}
+}
+
+// Destination sets destination for packet to be created.
+func (ipb *IPPacketBuilder) Destination(ip string) *IPPacketBuilder {
+	ipb.destination = net.ParseIP(ip)
+	return ipb
+}
+
+// Source sets source for packet to be created.
+func (ipb *IPPacketBuilder) Source(ip string) *IPPacketBuilder {
+	ipb.source = net.ParseIP(ip)
+	return ipb
+}
+
+// TTL sets ttl for packet to be created.
+func (ipb *IPPacketBuilder) TTL(ttl int) *IPPacketBuilder {
+	ipb.ttl = ttl
+	return ipb
+}
+
+// Protocol sets protocol for packet to be created.
+func (ipb *IPPacketBuilder) Protocol(protocol string) *IPPacketBuilder {
+	ipb.protocol = protocol
+	return ipb
+}
+
+// Build builds packet.
+func (ipb *IPPacketBuilder) Build() IPPacket {
+	return &ipPacket{ipb.destination, ipb.source, ipb.ttl, ipb.protocol}
+}
--- nsim/builders_test.go
+++ nsim/builders_test.go
@@ -35,3 +35,27 @@ func TestParseNetworkInterfaceFails(t *testing.T) {
 		t.Error("Expected to fail.")
 	}
 }
+
+func TestIPPacketBuilder(t *testing.T) {
+	packet := NewIPPacketBuilder().
+		Destination("192.168.1.1").
+		Source("192.168.2.2").
+		TTL(10).
+		Protocol("some-protocol").
+		Build()
+	if packet == nil {
+		t.Error("Should not be nil for correct config.")
+	}
+	if !packet.Destination().Equal(net.ParseIP("192.168.1.1")) {
+		t.Error("Destination was not set correctly.")
+	}
+	if !packet.Source().Equal(net.ParseIP("192.168.2.2")) {
+		t.Error("Source was not set correctly.")
+	}
+	if packet.TTL() != 10 {
+		t.Error("TTL was not set correctly.")
+	}
+	if packet.Protocol() != "some-protocol" {
+		t.Error("Protocol was not set correctly.")
+	}
+}
